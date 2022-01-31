ca5e07bc16d6887854d356df22a5570da3623f2d
Alexander Lyotov <sippie84@gmail.com>
2022-01-31 15:42:11 +0300

    chore/transfiguration was refactored

--- lermontovization/transfiguration.py
+++ lermontovization/transfiguration.py
@@ -1,23 +1,9 @@
 import logging
-from multiprocessing import process
 import os
-import sys
 import pymorphy2
-from corpy.udpipe import Model, pprint
+from corpy.udpipe import Model
 from preprod_research.tag_id_dict import shaping_grammemes, redundant_grammemes, pos_of_interest
 
-
-logger = logging.getLogger(__name__)
-logger.debug(sys.executable)
-
-
-logger = logging.getLogger(__name__)
-
-import re
-
-model_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'preprod_research/russian-syntagrus-ud-2.4-190531.udpipe'))
-input_text_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'source_text_sample.txt'))
-m = Model(model_path)
 
 MORPH = pymorphy2.MorphAnalyzer()
 INSANE = MORPH.parse('безумный')[0]
@@ -25,21 +11,24 @@ UNWORLDY = MORPH.parse('неземной')[0]
 UNEARTHLY = MORPH.parse('неотмирен')[0]  # Вспомогательный эпитет, используемый в качестве краткой формы второго эпитета
 EPITHETS = [INSANE, UNWORLDY]
 POETIC_FORMS = {'кратк': 'краток'}
+UDPIPE_MODEL = 'preprod_research/russian-syntagrus-ud-2.4-190531.udpipe'
+DEFAULT_TEXT_SAMPLE = 'source_text_sample.txt'
 
-# регулярка для поиска слов
-WORD_RE_COMPILED = re.compile(r'\w+')
+logger = logging.getLogger(__name__)
+
+model_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', UDPIPE_MODEL))
+input_text_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 
+                                  '..', 
+                                  DEFAULT_TEXT_SAMPLE))
+m = Model(model_path)
 
 
 def transfigurate_given_word(word, udpipe_word_tag, epithet_num=0):
-    print('---' * 30)
-    
     if word in POETIC_FORMS.keys():
         word = POETIC_FORMS[word]
 
     word_parse = MORPH.parse(word)
-    pprint(word_parse)
     if len(word_parse) > 1 and udpipe_word_tag != 'ADJ':
-        print(udpipe_word_tag)
         return None, None
 
     for p in word_parse:
@@ -47,10 +36,9 @@ def transfigurate_given_word(word, udpipe_word_tag, epithet_num=0):
             tag_str = str(p.tag)
             break
     else:
-        print(word)
-        print('Часть речи преобразуемого слова должна быть "прилагательное" или  "краткое прилагательное"')
+        logger.debug(word)
+        logger.debug('Часть речи преобразуемого слова должна быть "прилагательное" или  "краткое прилагательное"')
         return None, None
-
 
     tag_str = tag_str.replace(',', ' ')
     grammemes = tag_str.split()
@@ -66,13 +54,13 @@ def transfigurate_given_word(word, udpipe_word_tag, epithet_num=0):
         else:
             unexplored_grammemes.append(grammem)
 
-    print('Граммемы: ')
-    print('Формы: {};'.format(', '.join(target_form_grammemes)))
+    logger.debug('Граммемы: ')
+    logger.debug('Формы: %;' % ', '.join(target_form_grammemes))
     if word_redundant_grammemes:
-        print('Не влияющие на форму: {};'.format(', '.join(word_redundant_grammemes)))
+        logger.debug('Не влияющие на форму: %s;' % ', '.join(word_redundant_grammemes))
     if unexplored_grammemes:
-        print('Внимание, слову соответствуют следующие неисследованные автором программы лексеммы:')
-        print(', '.join(unexplored_grammemes))
+        logger.debug('Внимание, слову соответствуют следующие неисследованные автором программы лексеммы:')
+        logger.debug(', '.join(unexplored_grammemes))
 
     target_form_grammemes = set(target_form_grammemes)
 
@@ -81,6 +69,7 @@ def transfigurate_given_word(word, udpipe_word_tag, epithet_num=0):
         epithet = UNEARTHLY
 
     return (epithet, epithet.inflect(target_form_grammemes).word)
+
 
 def process_text_file(source_text_file_path):
     with open(source_text_file_path, 'r') as source_text_file:
@@ -96,7 +85,6 @@ def process_text(text_str):
     result_text = ''
     for sent in sents:
         for word in sent.words:
-            pprint(word)
             if word.id == 0:
                 continue
 
@@ -108,7 +96,9 @@ def process_text(text_str):
                 if word.form[0].isupper():
                     capitalized = True
 
-            epithet, new_word = transfigurate_given_word(word.form, word.upostag, epithet_num=epithet_n)
+            epithet, new_word = transfigurate_given_word(word.form, 
+                                                         word.upostag, 
+                                                         epithet_num=epithet_n)
             if new_word:
                 if all_upper:
                     new_word = new_word.upper()
@@ -127,30 +117,4 @@ def process_text(text_str):
                     result_text += ' '
             else:
                 result_text += ' '
-        # result_text += ' '
     return result_text
-
-#print(process_text_line('День немыслимо прекрасен, небо чисто и светло', 0))
-#sents = list(m.process("День немыслимо прекрасен, небо чисто и светло. Очерк жизни кратк и ясен: правда побеждает зло. Бледнее стал мой друг беспечный."))
-
-# import inspect
-
-# pprint(sents)
-# for sent in sents:
-#     #print(dir(sent))
-#     for word in sent.words:
-#         word.lemma = 'зуд'
-#         word.form = 'зудёж'
-#         pprint(word)
-#         # print(dir(word))
-#         # pprint(word.feats)
-#         # print(type(word))
-
-#         break
-    
-#     print(sent.getText())
-#     break
-# pprint(sents)
-print(process_text("День немыслимо прекрасен, небо чисто и светло. Очерк жизни кратк и ясен: правда побеждает зло. Бледнее стал мой друг беспечный."))
-
-#print(process_text_file(input_text_path))
